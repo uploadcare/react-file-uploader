724cdf38a598466de61ec23bdc564f9fd41ac350
Egor Didenko <didenk.egor@gmail.com>
2025-05-07 14:22:22 +0400

    chore(react-uploader): updated types for nextjs

--- packages/react-uploader/src/Uploader/Regular/NextFileUploaderRegular.ts
+++ packages/react-uploader/src/Uploader/Regular/NextFileUploaderRegular.ts
@@ -1,6 +1,8 @@
 import dynamic from "next/dynamic";
+import { TProps } from "../types";
+import { ComponentType } from "react";
 
-export const FileUploaderRegular = dynamic(
+export const FileUploaderRegular: ComponentType<TProps<"Regular">> = dynamic(
   () => import("./FileUploaderRegular").then((mod) => mod.FileUploaderRegular),
-  { ssr: false },
+  { ssr: false }
 );
