fe308712e135180f3e7d5e830fb1148506ca7f74
nd0ut <nd0ut@users.noreply.github.com>
2025-10-30 15:47:12 +0300

    chore: improve demo

--- packages/react-uploader/src/demo/index.tsx
+++ packages/react-uploader/src/demo/index.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { FileUploaderRegular } from "../libs";
+import {
+  FileUploaderInline,
+  FileUploaderMinimal,
+  FileUploaderRegular,
+} from "../libs";
 
 const App = () => {
-  return <FileUploaderRegular pubkey="demopublickey" />;
+  return (
+    <>
+      <FileUploaderRegular pubkey="demopublickey" />
+      <br />
+      <br />
+      <FileUploaderInline pubkey="demopublickey" />
+      <br />
+      <br />
+      <FileUploaderMinimal pubkey="demopublickey" />
+    </>
+  );
 };
 
 // biome-ignore lint/style/noNonNullAssertion: This is demo
